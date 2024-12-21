21e4b5fcaa014bfec5a6d3d6aabfd9e09c9f3f7e
Chinatsu0131 <xiaolinq509@gmail.com>
2024-12-21 15:50:25 +0900

    add test

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -2,7 +2,6 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-
 rclpy.init()
 node = Node("listener")
 
@@ -11,6 +10,9 @@ def cb(msg):
     global node
     node.get_logger().info("Listen: %d" % msg.data)
 
+    
 def main():
     pub = node.create_subscription(Int16, "countup", cb, 10)
     rclpy.spin(node)
+
+
