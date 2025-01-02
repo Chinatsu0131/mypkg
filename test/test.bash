fe975dd6e68a92b998500f01aa2f80a5b0124a56
Chinatsu0131 <xiaolinq509@gmail.com>
2025-01-02 23:07:44 +0900

    new test

--- test/test.bash
+++ test/test.bash
@@ -7,7 +7,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 20 ros2 launch mypkg cpu_monitor.launch.py > /tmp/mypkg.log
+timeout 30 ros2 launch mypkg cpu_monitor.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep 'Publishing CPU load' /tmp/mypkg.log || { echo "Expected output not found"; exit 1; }
+grep -E 'Publishing CPU load' /tmp/mypkg.log || { echo "Expected output not found"; exit 1; }
